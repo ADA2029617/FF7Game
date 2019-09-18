9712cdb0efa4521f8189103b5c538819a28716c5
Adan Partida <ada2029617@maricopa.edu>
2019-09-18 11:18:02 -0700

    Initial Commit

---
+++ FF7Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FF7Game
+//
+//  Created by Partida,Adan on 9/18/19.
+//  Copyright © 2019 Partida,Adan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
